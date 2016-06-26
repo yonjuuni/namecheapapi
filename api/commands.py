b02b39eba16d14d816e259a1b7494ee9cd301f56
Alex Sanchez <alex@s1ck.org>
2016-06-26 21:23:34 +0300

    add Session error collection and some docstrings

---
+++ api/commands.py
@@ -0,0 +1,7 @@
+"""A list of NC API commands.
+"""
+
+# General domain-related commands.
+DOMAINS_GET_INFO = 'namecheap.domains.getinfo'
+DOMAINS_GET_TLD_LIST = 'namecheap.domains.getTldlist'
+DOMAINS_CHECK = 'namecheap.domains.check'
--- api/session.py
+++ api/session.py
@@ -1,6 +1,12 @@
+"""
+"""
+
 from urllib.parse import urlencode
 from urllib.request import urlopen
 from xml.etree.ElementTree import fromstring
+from xml.etree.ElementTree import Element
+from api.exceptions import NCApiError
+from pprint import pprint
 
 
 URLS = {
@@ -8,19 +14,39 @@ URLS = {
     'sandbox': 'https://api.sandbox.namecheap.com/xml.response?'
 }
 
+NAMESPACE = 'http://api.namecheap.com/xml.response'
+
 
 class Session:
+    """Session class.
+
+    Defines the basic connection parameters and has several methods to
+    process the information received via API.
+    """
 
     def __init__(self, api_user: str, api_key: str, username: str,
                  client_ip: str, sandbox: bool = True) -> None:
+        """API initialization.
+
+        Arguments:
+        api_user -- NC API username.
+        api_key -- NC API key.
+        username -- your Namecheap username (in most cases).
+        client_ip -- public IP address of the machine making API calls
+            (MUST be whitelisted in your Namecheap account).
+        sandbox -- optional argument. Should be set to True for testing
+            and False on production.
+        """
         self.api_user = api_user
         self.api_key = api_key
         self.username = username
         self.client_ip = client_ip
         self.url = URLS['sandbox' if sandbox else 'production']
+        self.errors = []
 
     @property
     def _base_params(self) -> dict:
+
         return {
             'ApiUser': self.api_user,
             'ApiKey': self.api_key,
@@ -28,14 +54,72 @@ class Session:
             'ClientIp': self.client_ip,
         }
 
-    def _call(self, command: str, query: dict = {}) -> str:
-        url = self.url + urlencode({
+    def _form_url(self, command: str, query: dict) -> str:
+
+        return self.url + urlencode({
             **self._base_params,
             "Command": command,
             **query
         })
-        print(url)
-        return urlopen(url).read().decode('utf-8')
 
-    def _parse_xml(self):
-        pass
+    def _call(self, command: str, query: dict = {},
+              raw: bool = False)-> Element:
+
+        url = self._form_url(command, query)
+        pprint(url)  # debug
+        xml = fromstring(urlopen(url).read().decode('utf-8'))
+        if xml.attrib['Status'] == 'ERROR':
+            self._add_error(xml)
+            raise NCApiError(self.errors[-1])
+        else:
+            if raw:
+                return xml
+            return xml.find(self._tag('CommandResponse'))
+
+    def _tag(self, tag: str) -> str:
+        return '{{{}}}{}'.format(NAMESPACE, tag)
+
+    def _add_error(self, xml: Element) -> None:
+
+        data = {
+            'Errors': [],
+            'Warnings': []
+        }
+
+        for error in xml.find(self._tag('Errors')).findall(self._tag('Error')):
+            data['Errors'].append({
+                'Number': error.attrib['Number'],
+                'Text': error.text
+            })
+
+        for warning in xml.find(self._tag('Warnings')).findall(
+                self._tag('Warning')):
+            data['Warnings'].append({
+                'Number': warning.attrib['Number'],
+                'Text': warning.text
+            })
+
+        command = xml.find(self._tag('RequestedCommand'))
+        data['RequestedCommand'] = command.text
+
+        server = xml.find(self._tag('Server'))
+        data['Server'] = server.text
+
+        exectime = xml.find(self._tag('ExecutionTime'))
+        data['ExecutionTime'] = float(exectime.text)
+
+        self.errors.append(data)
+
+    def raw_query(self, command: str = '', query: dict = {}) -> str:
+        """Create a custom query.
+
+        Base parameters (API key, API user, etc.) are included by
+        default.
+
+        Arguments:
+        query -- dict with key/value pairs for GET request.
+
+        Returns:
+        raw XML string.
+        """
+        return self._call(command, query)
